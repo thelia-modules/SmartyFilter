6d122a6b956af35a9fee42a51c2518152053fccb
apenalver <apenalver@openstudio.fr>
2015-05-13 09:19:56 +0200

    remove smarty filter from backend default template

--- Parser/SmartyParser.php
+++ Parser/SmartyParser.php
@@ -13,7 +13,13 @@
 
 namespace SmartyFilter\Parser;
 
+use SmartyFilter\Model\SmartyFilterQuery;
 use TheliaSmarty\Template\SmartyParser as TheliaSmartyParser;
+use \Symfony\Component\HttpFoundation\Request;
+use \Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Thelia\Core\Template\ParserContext;
+use Thelia\Core\Template\TemplateHelperInterface;
+use Symfony\Component\DependencyInjection\Container;
 
 /**
  * Class SmartyParser
@@ -22,15 +28,60 @@ use TheliaSmarty\Template\SmartyParser as TheliaSmartyParser;
 class SmartyParser extends TheliaSmartyParser
 {
 
+    protected $container;
+
+    public function __construct(
+        Request $request,
+        EventDispatcherInterface $dispatcher,
+        ParserContext $parserContext,
+        TemplateHelperInterface $templateHelper,
+        $env = "prod",
+        $debug = false,
+        Container $container
+    ) {
+        $this->container = $container;
+        parent::__construct($request, $dispatcher, $parserContext, $templateHelper, $env, $debug);
+    }
+
     /**
      *
      * @inheritdoc
      */
-    public function fetch($template = null, $cache_id = null, $compile_id = null, $parent = null, $display = false, $merge_tpl_vars = true, $no_output_filter = false)
-    {
+    public function fetch(
+        $template = null,
+        $cache_id = null,
+        $compile_id = null,
+        $parent = null,
+        $display = false,
+        $merge_tpl_vars = true,
+        $no_output_filter = false
+    ) {
         if ($this->getTemplateDefinition()->getPath() === "backOffice/default") {
-            $no_output_filter = true;
+            $this->disableSmartyFilter();
         }
-        return parent::fetch($template, $cache_id, $compile_id, $parent, $display, $merge_tpl_vars, $no_output_filter);
+        $output = parent::fetch($template, $cache_id, $compile_id, $parent, $display, $merge_tpl_vars,
+            $no_output_filter);
+
+        if ($this->getTemplateDefinition()->getPath() === "backOffice/default") {
+            $this->enableSmartyFilter();
+        }
+
+        return $output;
+    }
+
+    public function disableSmartyFilter()
+    {
+        $filters = SmartyFilterQuery::create()->find();
+        foreach ($filters as $filter) {
+            $this->unregisterFilter('output', array(get_class($this->container->get($filter->getCode())), "filter"));
+        }
+    }
+
+    public function enableSmartyFilter()
+    {
+        $filters = SmartyFilterQuery::create()->find();
+        foreach ($filters as $filter) {
+            $this->registerFilter('output', array(get_class($this->container->get($filter->getCode())), "filter"));
+        }
     }
 }
